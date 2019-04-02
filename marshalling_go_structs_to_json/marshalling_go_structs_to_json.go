63e513c550abdf81054b818c43c772bdffa77e56
Leo <leogutierrezramirez@gmail.com>
2019-04-02 10:25:13 -0700

    marshalling

---
+++ marshalling_go_structs_to_json/marshalling_go_structs_to_json.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	Message string `json:"message"`
+	// Do not output the following field:
+	Author string `json:"-"`
+	// Do not output the following field if the value is empty:
+	Date string `json:",omitempty"`
+	// convert output to a string and rename "id"
+	ID int `json:"id,string"`
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "HelloWorld", Author: "Leo", Date: "789sdf", ID: 23}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("oops!")
+	}
+	fmt.Fprint(w, string(data))
+}
+
+func main() {
+	port := 8080
+	http.HandleFunc("/hello", helloWorldHandler)
+
+	// Using nil since we are using a default router ...
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
