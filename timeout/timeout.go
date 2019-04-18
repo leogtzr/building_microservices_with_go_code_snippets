0f5a04abe168be1f0caf422248041049ed125dbd
lgutierr <leonardo.gutierrez@autozone.com>
2019-04-18 15:01:09 -0600

    deadline example

--- timeout/timeout.go
+++ timeout/timeout.go
@@ -1,15 +1,30 @@
 package main
 
-func makeTimeoutRequest() { 
-	dl := deadline.New(1 * time.Second) 
+import (
+	"fmt"
+	"time"
+
+	"github.com/eapache/go-resiliency/deadline"
+)
+
+func slowFunction() {
+	time.Sleep(time.Millisecond * 3)
+}
+
+func makeTimeoutRequest() {
+	dl := deadline.New(1 * time.Second)
 	err := dl.Run(func(stopper <-chan struct{}) error {
-		slowFunction() 
-		return nil 
-	}) 
-	switch err { 
-		case deadline.ErrTimedOut: 
-			fmt.Println("Timeout") 
-		default: 
-			fmt.Println(err) 
-	} 
-} 
+		slowFunction()
+		return nil
+	})
+	switch err {
+	case deadline.ErrTimedOut:
+		fmt.Println("Timeout")
+	default:
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	makeTimeoutRequest()
+}
