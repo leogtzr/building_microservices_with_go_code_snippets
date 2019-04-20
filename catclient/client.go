4c54f33dcf022c26d005b57e97fe0f1f6ac0b6cb
leogtzr <leogutierrezramirez@gmail.com>
2019-04-20 13:41:11 -0600

    add notes file

--- catclient/client.go
+++ catclient/client.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"log"
 
-	proto "github.com/nicholasjackson/building-microservices-in-go/chapter6/grpc/proto"
+	proto "github.com/leogtzr/building_microservices_with_go_code_snippets/catclient/proto"
 	context "golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:9000", grpc.WithInsecure())
+	conn, err := grpc.Dial("127.0.0.1:8091", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal("Unable to create connection to server: ", err)
 	}
 
 	client := proto.NewKittensClient(conn)
-	response, err := client.Hello(context.Background(), &proto.Request{Name: "Nic"})
+	response, err := client.Hello(context.Background(), &proto.Request{Name: "Leo"})
 
 	if err != nil {
 		log.Fatal("Error calling service: ", err)
